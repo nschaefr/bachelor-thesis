9da7a84cad9d4bc8269c4d9c1cfe07952ddb027f
Nils Schäfer <nils.schaefer@tuta.io>
2024-06-17 16:30:51 +0200

    remove code

--- Assets/Code/test.c
+++
@@ -1,9 +0,0 @@
-    void main() {
-        const char *first_string = "abc"; //Definieren eines Strings
-        const char *second_string = "abc";
-        int result = mx_strcmp(first_string, second_string);
-
-        if (result != 0) {
-            printf("Vergleich schlug fehl!\n");
-        }
-    }
